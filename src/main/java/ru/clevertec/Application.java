0b9b5c0685b04b5f4a3ba36fba1d7b658c98ed4c
Korolenko Sergey <skorolenko@clevertec.ru>
2023-03-21 13:39:22 +0300

    init commit

---
+++ src/main/java/ru/clevertec/Application.java
@@ -0,0 +1,13 @@
+package ru.clevertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Go to http://localhost:8888/monitors");
+    }
+}
